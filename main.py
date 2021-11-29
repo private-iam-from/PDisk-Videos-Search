359498bb2f965ee12ed16b0499418d0579b7f3ee
private-iam-from <94742934+private-iam-from@users.noreply.github.com>
2021-11-29 20:10:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,8 +85,9 @@ async def text_handler(_, m: Message):
             if count > Configs.MAX_RESULTS:
                 break
             count += 1
-            text += f"**Title:** `{data[i]['title']}`\n" \
-                    f"**Description:** `{data[i]['description']}`\n" \
+            text += reply_markup=InlineKeyboardMarkup([
+                                     [InlineKeyboardButton("title", url="**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n")]
+                    
                     f"**PDisk Link:** {Configs.PDISK_DOMAIN + 'share-video?videoid=' + data[i]['share_link'].split('=', 1)[-1]}\n\n"
         try: await editable.edit(text, disable_web_page_preview=True)
         except MessageNotModified: pass
